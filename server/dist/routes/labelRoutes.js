10fdac45419ebbe0de6278d7f885007f530e03bd
John Hart <71567688+jhartlat@users.noreply.github.com>
2025-02-02 01:14:00 -0600

    feat: add labels component and route

---
+++ server/dist/routes/labelRoutes.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const router = express_1.default.Router();
+const labelControllers_1 = require("../controllers/labelControllers");
+// Routes
+router.post('/', labelControllers_1.createLabel);
+exports.default = router;
